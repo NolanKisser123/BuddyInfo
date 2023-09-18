2e065f37679f31ce3b13b580a47aaad2cbf8dbdd
Nolan <nolankisser@cmail.carleton.ca>
2023-09-17 22:05:25 -0400

    Output a string

---
+++ src/AddressBook.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AddressBook {
+    public ArrayList<BuddyInfo> collection = new ArrayList<BuddyInfo>();
+
+    public void addBuddy(){
+
+    }
+    public void removeBuddy(){
+
+    }
+
+    public static void main(String[] args){
+        System.out.println("Address Book");
+    }
+}
