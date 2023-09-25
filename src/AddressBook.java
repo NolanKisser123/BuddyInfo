3da538a4f45273b7c4c30e90bc80bff3e0315759
Nolan <nolankisser@cmail.carleton.ca>
2023-09-25 12:26:22 -0400

    Made BuddyInfo object and tested addBuddy and removeBuddy

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -20,5 +20,6 @@ public class AddressBook {
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(0);
+        System.out.print("TESTING A BRANCH");
     }
 }
